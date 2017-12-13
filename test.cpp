f19d8f8fe67228314acd4d282cad611a7d4141d6
Florian Zylla <florian.zylla@gmail.com>
2017-12-13 15:57:53 +0100

    test

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+ int main()
+ {
+    float i=33.33;
+    cout.width(8);
+    cout.fill('-');
+    cout.setf(ios::left);
+    cout <<i <<endl;
+    return 0;
+
+
+ }
